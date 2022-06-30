0d48ec21085a647be788425ccdb4828c8cd4acb1
Cem Tanrikut <cemtanrikut@gmail.com>
2022-06-30 22:48:44 +0300

    Add getMinPurchaserCount func

--- api/product.go
+++ api/product.go
@@ -6,18 +6,26 @@ type Product_Model struct {
 	Product_ID  string
 	Name        string
 	Description string
+	Price       float32
 	Fee         float32
 	Create_Date time.Time
 }
 
 type Draw_Model struct {
-	Draw_ID         string
-	Product_Model   Product_Model
-	Start_Date      time.Time
-	End_Date        time.Time
-	Is_Active       bool
-	Min_Purchasers  int
-	Total_Purchaser int
-	Status          string
-	Winner_User     string //User model must be here
+	Draw_ID             string
+	Product_Model       Product_Model
+	Start_Date          time.Time
+	End_Date            time.Time
+	Is_Active           bool
+	Min_Purchaser_Count int
+	Total_Purchaser     int
+	Status              string
+	Winner_User         string //User model must be here
+}
+
+func getMinPurchaserCount(productModel Product_Model) float32 {
+	productPrice := productModel.Price
+	productFee := productModel.Fee
+	expectedCount := productPrice + (productPrice * (productFee / 10))
+	return expectedCount
 }
