b64b68ca5fd4a7fe058bd53dbda7dd2b7068a085
Cem Tanrikut <cemtanrikut@gmail.com>
2022-06-30 23:06:26 +0300

    Update user api

--- api/user.go
+++ api/user.go
@@ -4,25 +4,38 @@ import "time"
 
 type User_Model struct {
 	User_ID              string
-	Name                 string
-	Surname              string
-	Email                string
-	Phone                string
-	Province             string
-	District             string
-	Address              string
+	User_SignIn          User_SignIn
+	Address              User_Address
 	Citizenship_Number   string
 	Is_Active            bool
 	Create_Date          time.Time
 	User_Draw_Data_Model User_Draw_Data_Model
 }
 
+//Address info
+type User_Address struct {
+	Phone    string
+	Province string
+	District string
+	Address  string
+}
+
+//Sign in info
+type User_SignIn struct {
+	Name     string
+	Surname  string
+	Email    string
+	Password string
+}
+
+//Datas from draw
 type User_Draw_Data_Model struct {
 	Join_Count int
 	Win_Count  int
 	Lost_Count int
 }
 
+//User's joined draws
 type User_Joined_Draw struct {
 	User_ID string
 	Draw_ID string
