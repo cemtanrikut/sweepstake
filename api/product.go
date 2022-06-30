9a2d20ef140f8927fffd1f0501d266e603df4f72
Cem Tanrikut <cemtanrikut@gmail.com>
2022-06-30 22:37:44 +0300

    Add Draw model

--- api/product.go
+++ api/product.go
@@ -9,3 +9,15 @@ type Product_Model struct {
 	Fee         float32
 	Create_Date time.Time
 }
+
+type Draw_Model struct {
+	Draw_ID         string
+	Product_Model   Product_Model
+	Start_Date      time.Time
+	End_Date        time.Time
+	Is_Active       bool
+	Min_Purchasers  int
+	Total_Purchaser int
+	Status          string
+	Winner_User     string //User model must be here
+}
